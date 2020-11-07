da51d6c27849edd55208b74bb1854caddd8cab6b
survani <giovanicode@outlook.com>
2020-11-06 19:53:50 -0800

    Finished ex2.py.

--- hashtables/ex2/ex2_tests.py
+++ hashtables/ex2/ex2_tests.py
@@ -6,13 +6,13 @@ from ex2 import Ticket, reconstruct_trip
 class TestEx2(unittest.TestCase):
 
     def test_short_case(self):
-        ticket_1 = Ticket("NONE", "PDX")
+        ticket_1 = Ticket("none", "PDX")
         ticket_2 = Ticket("PDX", "DCA")
-        ticket_3 = Ticket("DCA", "NONE")
+        ticket_3 = Ticket("DCA", "none")
 
         tickets = [ticket_1, ticket_2, ticket_3]
 
-        expected = ["PDX", "DCA", "NONE"]
+        expected = ["PDX", "DCA", "none"]
         result = reconstruct_trip(tickets, 3)
 
         self.assertTrue(expected == result)
@@ -22,10 +22,10 @@ class TestEx2(unittest.TestCase):
         ticket_2 = Ticket("XNA", "SAP")
         ticket_3 = Ticket("SFO", "BHM")
         ticket_4 = Ticket("FLG", "XNA")
-        ticket_5 = Ticket("NONE", "LAX")
+        ticket_5 = Ticket("none", "LAX")
         ticket_6 = Ticket("LAX", "SFO")
         ticket_7 = Ticket("SAP", "SLC")
-        ticket_8 = Ticket("ORD", "NONE")
+        ticket_8 = Ticket("ORD", "none")
         ticket_9 = Ticket("SLC", "PIT")
         ticket_10 = Ticket("BHM", "FLG")
 
@@ -33,7 +33,7 @@ class TestEx2(unittest.TestCase):
                    ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
 
         expected = ["LAX", "SFO", "BHM", "FLG", "XNA", "SAP",
-                    "SLC", "PIT", "ORD", "NONE"]
+                    "SLC", "PIT", "ORD", "none"]
         result = reconstruct_trip(tickets, 10)
 
         self.assertTrue(expected == result)
